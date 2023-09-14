c0eb5b9dd36797e3425bd1032236248304590325
KeepZoneqp <862800490@qq.com>
2023-09-13 22:17:22 -0400

    late work for lab1

---
+++ src/Reduce.java
@@ -0,0 +1,26 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int count = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+
+                n = n/2;
+            }
+            else {
+                n = n - 1;
+            }
+            count ++;
+
+        }
+        System.out.println("There are " + count + " steps.");
+
+
+    }
+}
+
+
+
+
+
+
